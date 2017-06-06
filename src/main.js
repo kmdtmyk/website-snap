ab8fd0a4e51f66751b8b2f3e98bacda61a8b331f
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-06-06 10:25:57 +0900

    Add filter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import filters from './filters'
 import components from './components'
 import routes from './routes'
+
+for(const name in filters){
+  Vue.filter(name, filters[name])
+}
 
 for(const name in components){
   Vue.component(name, components[name])
