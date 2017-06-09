21a012d3b0d52af37c28d1ce9e5d6d2cd52a1805
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-06-09 17:30:45 +0900

    Add start and end to log

--- src/views/snaps/SnapHistory/Logs.vue
+++ src/views/snaps/SnapHistory/Logs.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+    <div v-if='snapHistory.start'>
+      start: {{snapHistory.start | datetime}}
+    </div>
     <div v-for='(log, index) in snapHistory.logs' :key='index'>
       <span :class='log.status'>[{{log.status}}]</span>
       <span>{{log.url}}</span>
     </div>
     <div v-if='snapHistory.end'>
-      finished.
+      end: {{snapHistory.end | datetime}}
+      ({{(snapHistory.end - snapHistory.start) / 1000}} sec.)
     </div>
   </div>
 </template>
